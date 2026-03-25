b61406a56e7f3c272fd199ed5b5e6bd0d9955012
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2026-03-24 23:21:08 -0300

    correciones

--- api/views.py
+++ api/views.py
@@ -6,7 +6,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-items = [{"id": 1, "nombre": "Laptop"}, {"id": 2, "nombre": "Telefono"}, {"id": 3, "nombre": "Auriculares"}]
+items = [{"id": 1, "nombre": "Laptop"},
+         {"id": 2, "nombre": "Telefono"},
+         {"id": 3, "nombre": "Auriculares"}]
+
 
 # GET y POST
 @csrf_exempt
@@ -16,25 +19,20 @@ def obtener_agregar_items(request):
     elif request.method == 'POST':
         try:
             data = json.loads(request.body)
-            nuevo_item = {"id": len(items) + 1,
-                      "nombre": data.get("nombre", "Sin nombre")}
-            items.append (nuevo_item)
-            return JsonResponse(nuevo_item, status=201)  # Respuesta
+            nuevo_item = {"id": len(items) + 1, "nombre": data.get("nombre", "Sin nombre")}
+            items.append(nuevo_item)
+            return JsonResponse(nuevo_item, status=201)
         except json.JSONDecodeError:
             return JsonResponse({"error": "Formato JSON inválido"}, status=400)
+
 
 # PUT y DELETE
 @csrf_exempt
 def modificar_eliminar_item(request, id):
-    # Buscar item
-    item = next((i for i in items if i["id"] == id), None)
-
-    if not item:
-        return JsonResponse({"error": "Item no encontrado"}, status=404)
-
     if request.method == 'PUT':
         try:
             data = json.loads(request.body)
+
             for item in items:
                 if item["id"] == id:
                     item["nombre"] = data.get("nombre", item["nombre"])
@@ -50,3 +48,5 @@ def modificar_eliminar_item(request, id):
             if item["id"] == id:
                 items.remove(item)
                 return JsonResponse({"mensaje": "Item eliminado"})
+
+        return JsonResponse({"error": "Item no encontrado"}, status=404)
